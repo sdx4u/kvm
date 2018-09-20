5062b4c47710c1c2be760ea225399e37e205bf60
Jaehyun Nam <namjh@kaist.ac.kr>
2018-09-20 15:05:57 -0700

    Update kvm-install.sh

--- kvm-install.sh
+++ kvm-install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo apt-get install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils uuid \
                         linux-tools-common linux-tools-`uname -r` \
                         linux-tools-virtual virt-top virt-manager
