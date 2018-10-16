afc77150d9241d6b340d2d2ca0923ed6a35d9ce5
Jaehyun Nam <namjh@kaist.ac.kr>
2018-10-16 14:55:36 -0700

    Update ovs-setup.sh

--- ovs-setup.sh
+++ ovs-setup.sh
@@ -9,10 +9,6 @@ sudo ovs-vsctl set-controller ovsbr0 tcp:127.0.0.1:6633
 sudo ovs-vsctl -- set bridge ovsbr0 protocols=OpenFlow10
 sudo ovs-vsctl set-fail-mode ovsbr0 secure
 
-# add interfaces to ovsbr0
-#sudo ovs-vsctl add-port ovsbr0 p1p1
-#sudo ovs-vsctl add-port ovsbr0 p1p2
-
 virsh net-define ovsbr0.xml
 virsh net-start ovsbr0
 virsh net-autostart ovsbr0
