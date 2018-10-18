23f1dd121bf9855af36712b4f80e4ee53c23c3e9
Jaehyun Nam <namjh@kaist.ac.kr>
2018-10-19 06:57:41 +0900

    minor updates

--- download_ubuntu.sh
+++ download_ubuntu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-mkdir ~/images
-cd ~/images
+mkdir ~/images; cd ~/images
 wget http://www.sdx4u.net/downloads/ubuntu-16.04.2-server-amd64.iso
--- ovs-setup.sh
+++ ovs-setup.sh
@@ -9,10 +9,7 @@ sudo ovs-vsctl set-controller ovsbr0 tcp:127.0.0.1:6633
 sudo ovs-vsctl -- set bridge ovsbr0 protocols=OpenFlow10
 sudo ovs-vsctl set-fail-mode ovsbr0 secure
 
-# add interfaces to ovsbr0
-#sudo ovs-vsctl add-port ovsbr0 p1p1
-#sudo ovs-vsctl add-port ovsbr0 p1p2
-
+# add ovsbr0 into KVM
 virsh net-define ovsbr0.xml
 virsh net-start ovsbr0
 virsh net-autostart ovsbr0
