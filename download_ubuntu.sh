b7d3001d6c0cccc0c0696414203cfb89f92dbbec
Jaehyun Nam <67940721+sdxdata-namjh@users.noreply.github.com>
2020-08-27 15:45:58 +0900

    Update download_ubuntu.sh

--- download_ubuntu.sh
+++ download_ubuntu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir ~/images; cd ~/images
-wget http://www.sdx4u.net/downloads/ubuntu-16.04.2-server-amd64.iso
+wget http://www.sdxdata.com/downloads/ubuntu-16.04.2-server-amd64.iso
